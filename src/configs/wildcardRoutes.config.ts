a066b59fdc494e2c62953997b1f860b16a07450d
ramant <ramant@inqui-lab.org>
2025-02-03 10:24:47 +0530

    bug fix 03

--- src/configs/wildcardRoutes.config.ts
+++ src/configs/wildcardRoutes.config.ts
@@ -28,5 +28,6 @@ export const wildcardRoutes = [
     "/api/v1/students/forgotPassword",
     "/api/v1/evaluators/register",
     "/api/v1/evaluators/login",
-    "/api/v1/dashboard/CollegeNameForCollegeType"
+    "/api/v1/dashboard/CollegeNameForCollegeType",
+    "/api/v1/students/triggerWelcomeEmail"
 ];
